f3b2bf08041e1ff6549b9be0cf6b0aceb874f4db
mykdolnyk <mdolnyk@gmail.com>
2025-06-29 13:55:12 +0300

    Fixed site being unavailable for non-logged in users

--- users/helpers/context_processors.py
+++ users/helpers/context_processors.py
@@ -2,4 +2,8 @@ from django.http import HttpRequest
 
 
 def notification_count(request: HttpRequest):
-    return {'notification_count': request.user.notifications.filter(is_seen=False).count()}
+    if request.user.is_authenticated:
+        notifications = request.user.notifications.filter(is_seen=False).count()
+    else:
+        notifications = None
+    return {'notification_count': notifications}
