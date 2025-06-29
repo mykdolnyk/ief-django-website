489f6205591c1071be01a021a10bf446371f490b
mykdolnyk <mdolnyk@gmail.com>
2025-06-29 13:55:04 +0300

    Added BlankCharactersValidator and connected it

---
+++ users/helpers/password_validation.py
@@ -0,0 +1,17 @@
+from django.forms import ValidationError
+from django.utils.translation import gettext as _
+
+class BlankCharactersValidator:
+    """
+    Validate that the password doesn't have blank characters.
+    """
+
+    def validate(self, password: str, user=None):
+        if len(password.strip()) >= 1:
+            raise ValidationError(
+                _("This password contains blank characters."),
+                code="password_blank_spaces",
+            )
+        
+    def get_help_text(self):
+        return _("Your password can’t contain blank characters.")
