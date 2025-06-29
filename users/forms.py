3019eb860ded4a305d67d334f3268b529afed41d
mykdolnyk <mdolnyk@gmail.com>
2025-06-29 13:55:06 +0300

    Implemented login, register and logout views, forms and templates

---
+++ users/forms.py
@@ -0,0 +1,98 @@
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth.models import User
+from django.contrib.auth.password_validation import validate_password
+from django import forms
+from .helpers import mcuser
+
+
+class UserRegistrationFormOld(UserCreationForm):
+    email = forms.EmailField(required=True)
+    application = forms.CharField(max_length=256, required=False, widget=forms.Textarea)
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        
+        for field in self.fields:
+            self.fields[field].help_text = None
+            
+    def clean_application(self):
+        data = self.cleaned_data['application']
+        
+        if not data:
+            data = ''
+        
+        return data
+
+
+class UserRegistrationForm(forms.ModelForm):
+    
+    password1 = forms.CharField(
+        label="Password",
+        widget=forms.PasswordInput(attrs={"autocomplete": "new-password"}),
+        strip=False,
+    )
+    password2 = forms.CharField(
+        label="Password confirmation",
+        widget=forms.PasswordInput(attrs={"autocomplete": "new-password"}),
+        strip=False,
+    )
+    application = forms.CharField(max_length=256, required=False, widget=forms.Textarea)
+    
+    class Meta:
+        model = User
+        fields = ("username", 'password1', 'password2', 'email')
+        
+        help_texts = {
+            'username': None
+        }
+        
+    def clean_password1(self):
+        data = self.cleaned_data["password1"]
+
+        validate_password(data, User)
+
+
+        return data
+    
+    def clean_password2(self):
+        password1 = self.cleaned_data.get("password1")
+        password2 = self.cleaned_data.get("password2")
+        if password1 and password2 and password1 != password2:
+            raise forms.ValidationError(
+                "Passwords Mismatch",
+                code="password_mismatch",
+            )
+        return password2
+    
+    def clean_username(self):
+        data = self.cleaned_data["username"]
+        if User.objects.filter(username=data).exists():
+            raise forms.ValidationError(
+                "The username is already taken."
+            )
+            
+        if not mcuser.username_to_mc_uuid(data):
+            raise forms.ValidationError(
+                "Such Minecraft user does not exist."
+            )
+            
+        return data
+        
+    def clean_application(self):
+        data = self.cleaned_data['application']
+        
+        if not data:
+            data = ''
+        
+        return data
+    
+    def save(self, commit=False):
+        user = super().save(commit=False)
+        user.set_password(self.cleaned_data["password1"])
+        if commit:
+            user.save()
+            if hasattr(self, "save_m2m"):
+                self.save_m2m()
+        return user
+
+
--- users/views.py
+++ users/views.py
@@ -1,5 +1,12 @@
-from django.http import HttpRequest
-from django.shortcuts import render
+from django.http import HttpRequest, HttpResponse
+from django.shortcuts import redirect, render
+from django.urls import reverse
+from .forms import UserRegistrationForm, UserRegistrationFormOld
+from .models import RegistrationApplication, UserProfile, User
+from django.contrib.auth import login, logout, authenticate
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth.decorators import login_required
+from .helpers import mcuser
 
 # Create your views here.
 
@@ -35,13 +42,83 @@ def user_notifications(request: HttpRequest):
     pass
 
 
-def login(request: HttpRequest):
-    pass
+def register_page(request: HttpRequest):
+    """The registration page."""
+    if request.user.is_authenticated:
+        return redirect(reverse('logout_page'))
+    
+    context = {}
+    
+    if request.method == 'POST':
+        form = UserRegistrationForm(request.POST)
+        
+        if form.is_valid():
+            # Save the User, the Application and Profile instances
+            try:
+                user: User = form.save() # Save the user from the form to the instance
+                user.is_active = False # Make the client inactive until the application is reviewed
+                user.save() # Save the user from the instance to the DB (required to get the PK)
+
+                RegistrationApplication.objects.create(
+                    user = user,
+                    text = form.cleaned_data['application'],
+                ).save()
+                
+                UserProfile.objects.create(
+                    user=user,
+                    mcuuid=mcuser.username_to_mc_uuid(user.username),
+                ).save()
+                
+            except Exception as exc:
+                # In case some error occures, the User instance should be deleted as it was
+                # already saved, but the registration failed
+                user.delete()
+                form.add_error(field=None, error='Some unknown error occured. Please try again a bit later.')
+            
+    elif request.method == 'GET':
+        form = UserRegistrationForm()
+        
+    context['form'] = form
+
+    return render(request, 'users/logreg/register_page.html', context=context)
 
 
-def register(request: HttpRequest):
-    pass
+def login_page(request: HttpRequest):
+    if request.user.is_authenticated:
+        return redirect(reverse('logout_page'))
+    
+    context = {}
+    
+    if request.method == 'POST':
+        form = AuthenticationForm(data=request.POST)
 
+        if form.is_valid():
+            login(request, form.get_user())
+        # else:
+        #     # If User's application status is not 'reviewed', show the corresponind message:
+        #     if User.objects.get(username=form.cleaned_data['username']).application.get().status < 4:
+        #         form.add_error(field=None, error=form.error_messages['inactive'])
+            
 
-def logout(request: HttpRequest):
-    pass
+    elif request.method == 'GET':
+        form = AuthenticationForm()
+        
+    context['form'] = form
+    
+        
+    return render(request, 'users/logreg/login_page.html', context=context)
+    
+    
+@login_required
+def logout_page(request: HttpRequest):
+    context = {}
+    
+    if request.method == 'POST':
+        if request.POST.get('yes'):
+            logout(request)
+            return redirect(reverse('register_page'))
+        
+        elif request.POST.get('no'):
+            return redirect(reverse('user_page'))
+    
+    return render(request, 'users/logreg/logout_page.html', context=context)
