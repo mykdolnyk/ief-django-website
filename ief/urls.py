0cf90011b8b64d571aa53fd7a6deed868153dd33
mykdolnyk <mdolnyk@gmail.com>
2025-06-29 13:55:06 +0300

    Rearranged URLs

--- ief/urls.py
+++ ief/urls.py
@@ -1,22 +1,17 @@
-"""
-URL configuration for ief project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.0/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
+import users.views as userviews
+import blogs.views as blogviews
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('user/', include('users.urls')),
+    
+    path('', blogviews.index_page, name='index_page'),
+    
+    path('login/', userviews.login_page, name='login_page'),
+    path('register/', userviews.register_page, name='register_page'),
+    path('logout/', userviews.logout_page, name='logout_page'),
+    
 ]
--- users/urls.py
+++ users/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 
 urlpatterns = [
-    path('user/', views.user_list, name='user_list'),
+    path('', views.user_list, name='user_list'),
     path("user/<int:id>/", views.user_page, name="user_page"),
     path("user/<int:id>/edit/", views.user_edit, name="user_edit"),
     path("user/<int:id>/awards/", views.user_award_list, name="user_award_list"),
@@ -11,8 +11,4 @@ urlpatterns = [
     path("user/<int:id>/media/", views.user_media_list, name="user_media_list"),
     path("user/<int:id>/posts/", views.user_post_list, name="user_post_list"),
     path("user/notifications/", views.user_notifications, name="user_notifications"),
-    
-    path('login/', views.login_page, name='login_page'),
-    path('register/', views.register_page, name='register_page'),
-    path('logout/', views.logout_page, name='logout_page'),
 ]
