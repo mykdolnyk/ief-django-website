20e791b987c2558b0a955970aeac1666daa5a1de
mykdolnyk <mdolnyk@gmail.com>
2025-06-29 13:55:03 +0300

    Prototyped UserProfile and ProfileComment models

--- users/models.py
+++ users/models.py
@@ -1,3 +1,25 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+class UserProfile(models.Model):
+    user = models.ForeignKey(User)
+    username = models.CharField(verbose_name='Minecraft Username')
+    bio = models.CharField(verbose_name='Bio', max_length=512, default='')
+    signing = models.CharField(verbose_name='Signing', max_length=64, default='')
+    
+    pfp = models.ImageField(verbose_name='Profile picture')
+    
+    def __str__(self):
+        return f'<{self.username} Profile>'
+
+
+class ProfileComment(models.Model):
+    profile = models.ForeignKey(UserProfile, verbose_name=UserProfile, on_delete=models.CASCADE)
+    text = models.CharField(max_length=512)
+    is_visible = models.BooleanField(default=False)
+    
+    def __str__(self):
+        return f'<{self.profile.username} Comment: {'not' if self.is_visible else ''} visible>'
+    
